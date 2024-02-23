43e215ddee5f23082ed45fff80bc5bcb4fd83c70
Danish Malik <cyanide9102@gmail.com>
2024-02-23 15:49:47 +0300

    solved top K frequent elements problem

---
+++ Problem5_TopKFrequentElements.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeProblems
+{
+    public static class Problem5_TopKFrequentElements
+    {
+        public static int[] TopKFrequent(int[] nums, int k)
+        {
+            if (nums.Length == 1 && k == 1)
+            {
+                return [nums[0]];
+            }
+
+            Dictionary<int, int> numsFrequency = [];
+            foreach (int num in nums)
+            {
+                if (numsFrequency.TryGetValue(num, out int frequency))
+                {
+                    ++numsFrequency[num];
+                }
+                else
+                {
+                    numsFrequency.Add(num, 1);
+                }
+            }
+
+            List<(int, int)> numsFrequencyTopK = numsFrequency.OrderByDescending(x => x.Value)
+                                                              .Take(k)
+                                                              .Select(kvp => (kvp.Key, kvp.Value))
+                                                              .ToList();
+
+            int[] numsTopK = new int[k];
+            int index = 0;
+            foreach ((int, int) topK in numsFrequencyTopK)
+            {
+                numsTopK[index] = topK.Item1;
+                ++index;
+            }
+
+            return numsTopK;
+        }
+    }
+}
