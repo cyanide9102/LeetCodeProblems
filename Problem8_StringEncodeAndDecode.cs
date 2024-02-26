fbbd761dfee964bffc91dc9ae1a2bddfd1023912
Danish Malik <cyanide9102@gmail.com>
2024-02-26 21:01:30 +0300

    implemented string encode and decode problem

---
+++ Problem8_StringEncodeAndDecode.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeProblems;
+public class Problem8_StringEncodeAndDecode
+{
+    public static string Encode(IList<string> strs)
+    {
+        string encodedString = string.Empty;
+        foreach (string str in strs)
+        {
+            encodedString += str;
+            encodedString += "][><][";
+        }
+
+        return encodedString;
+    }
+
+    public static List<string> Decode(string s)
+    {
+        string splitter = @"][><][";
+        List<string> strs = [.. s.Split(splitter, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)];
+        return strs;
+    }
+}
