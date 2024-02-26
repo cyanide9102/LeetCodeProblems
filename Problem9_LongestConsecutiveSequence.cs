b2d9a2fd611b52d3492a0780bac74de381b50a62
Danish Malik <cyanide9102@gmail.com>
2024-02-27 00:50:05 +0300

    implemented longest consecutive sequence problem

---
+++ Problem9_LongestConsecutiveSequence.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeProblems;
+public static class Problem9_LongestConsecutiveSequence
+{
+    public static int LongestConsecutive(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        HashSet<int> set = new(nums);
+        int maxCount = 1;
+        foreach (int num in set)
+        {
+            if (set.Contains(num - 1))
+            {
+                continue;
+            }
+
+            int count = 1;
+            while (set.Contains(num + count))
+            {
+                ++count;
+            }
+
+            maxCount = Math.Max(count, maxCount);
+        }
+
+        return maxCount;
+    }
+}
