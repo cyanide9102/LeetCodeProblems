532643b2588b980fe021be1bff2b30356ed8face
Danish Malik <cyanide9102@gmail.com>
2024-02-29 01:55:23 +0300

    improved solution for problem 11 using two pointers

--- Problem011_TwoSumII.cs
+++ Problem011_TwoSumII.cs
@@ -5,28 +5,22 @@ public static class Problem011_TwoSumII
     public static int[] TwoSum(int[] numbers, int target)
     {
         int i = 0;
-        int upperLimit = numbers.Length - 1;
-        while (i < numbers.Length - 1)
+        int j = numbers.Length - 1;
+        while (i < j)
         {
-            int j = i + 1;
-            while (j <= upperLimit)
+            int twoSum = numbers[i] + numbers[j];
+            if (twoSum > target)
             {
-                int twoSum = numbers[i] + numbers[j];
-                if (twoSum == target)
-                {
-                    return [i + 1, j + 1];
-                }
-                else if (twoSum > target)
-                {
-                    upperLimit = j;
-                    break;
-                }
-                else
-                {
-                    ++j;
-                }
+                --j;
             }
-            ++i;
+            else if (twoSum < target)
+            {
+                ++i;
+            }
+            else
+            {
+                return [i + 1, j + 1];
+            }
         }
 
         throw new Exception("No solution found!");
